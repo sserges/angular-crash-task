9c069a2f0d2258c8449e8f34bb1ec7a539e5dbc3
sserges <agounkeserge@gmail.com>
2021-06-01 01:19:00 +0000

    Create a Task Service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskService } from '../../services/task.service';
+
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -9,9 +10,11 @@ import { TASKS } from '../../mock-tasks';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
 }
